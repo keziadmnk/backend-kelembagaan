ca6ebe41d4275ef7e7da40da35d9124a15fc4b71
keziadmnk <keziadamanik20@gmail.com>
2026-04-24 11:38:15 +0700

    rev db config

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD || '',
   {
     host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 5432,
+    port: process.env.DB_PORT || 5433,
     dialect: 'postgres',
     logging: false
   }
